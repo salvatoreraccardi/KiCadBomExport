36e2ef05c2cf0b624fa2575ae0a110784789ab75
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-02-03 19:50:30 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
 import sys
-import builder
-
-
-#print('Number of arguments:', len(sys.argv), 'arguments.')
-#print('Argument List:', str(sys.argv))
-
-#print(sys.argv[2])
+import builder 
 
 #command example:  test.py <bom_file.xml> <CSV | .XLSX> <id_template> <name_new_file>
 
 try:
-    if 'csv' in sys.argv[2]:
-        builder.csvTool(sys.argv[1], sys.argv[3], sys.argv[4])
-    if 'xlsx' in sys.argv[2]:
-        builder.xlsxTool(sys.argv[1], sys.argv[3], sys.argv[4])
-    if not 'csv' and 'xlsx' in sys.argv[2]:
-        print('Oops, extensions available: .CVS or .XLSX')
+    if sys.argv[1].lower().endswith(('.xml')):
+
+        if 'csv' == sys.argv[2]:
+            print('CSV')
+            #builder.csvTool(sys.argv[1], sys.argv[3], sys.argv[4])
+        if 'xlsx' == sys.argv[2]:
+            print('XLSX')
+            #builder.xlsxTool(sys.argv[1], sys.argv[3], sys.argv[4])
+        else:
+            print('ERROR - WRONG COMMAND')
+            print('example:  run.py <bom_file.xml> <CSV or XLSX> <id_template> <name_file>')
+    else:
+        print('ONLY .XML FILES!')
         
     #print('Your command: ' + sys.argv[1] + ' | ' + sys.argv[2] + ' | ' + sys.argv[3])
 
